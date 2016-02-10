1cf1e48f8102b13d6ba6ddc6d62a18d116106b70
AndrewNaylor <anaylor@gmail.com>
2016-02-10 22:06:40 +0000

    adding teens

--- string.pl
+++ string.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl
+#
+# This script is for generating lists of strings that can be used to register domain names.
+#
 use strict;
 use warnings;
 use Set::CrossProduct;
@@ -6,13 +9,23 @@ use Set::CrossProduct;
 my $debug = 0;
 my $reply="";
 if ( $ARGV[0] ) { $reply = $ARGV[0]; }
+# This is laid out to better understand the differences between the letter lists. This first
+# one is for all letters, A-Z.
 my $letters = [ qw( a b c d e f g h i j k l m n o p q r s t u v w x y z ) ];
+# This pinyin list is the favored letters for Chinese domain investors.
 my $pinyin  = [ qw(   b c d   f g h   j k l m n   p q r s t     w x y z ) ];
+# These are the most desired two-letter Pinyin prefixes.
 my $prefix  = [ qw( cn bj sh gd ) ];
+# These are the most desired two-letter Pinyin suffixes.
 my $suffix  = [ qw( cn lc zx kj sj ) ];
+# Numbers.
 my $number1 = [ ( 0 .. 9 ) ];
+# Numbers spelled out.
 my $number2 = [ qw( zero one two three four five six seven eight nine ) ];
-my $number3 = [ qw( ten twenty thirty fourty fifty sixty seventy eighty ninety ) ];
+# Single word number words.
+my $number3 = [ qw( eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen ) ];
+# Single word number words, product of ten.
+my $number4 = [ qw( ten twenty thirty fourty fifty sixty seventy eighty ninety hundred ) ];
 
 if ( !$ARGV[0] ) {
 	print "What pattern would you like to search? ";
@@ -38,7 +51,8 @@ for my $c ( 0 .. $#chars ) {
 	elsif ( $chars[$c] =~ m/E/ ) { $carray[$c]    = $prefix;    }
 	elsif ( $chars[$c] =~ m/U/ ) { $carray[$c]    = $suffix;    }
 	elsif ( $chars[$c] =~ m/Z/ ) { $carray[$c]    = $number2;   }
-	elsif ( $chars[$c] =~ m/T/ ) { $carray[$c]    = $number3;   }
+	elsif ( $chars[$c] =~ m/V/ ) { $carray[$c]    = $number3;   }
+	elsif ( $chars[$c] =~ m/T/ ) { $carray[$c]    = $number4;   }
 	else                         { $carray[$c][0] = $chars[$c]; };
 	print "$c - $chars[$c]\n" if $debug > 1;
 }
